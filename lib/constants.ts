10406a9f23243cdf54d0ad06c13708391962f5c4
tasneemtantawy <tasnim0tantawi@gmail.com>
2023-09-21 11:58:48 +0300

    ..

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const usersEndpoint = "http://127.0.0.1:8000/api/v1/users";
-// export const usersEndpoint = "https://dummyjson.com/users";
+// export const usersEndpoint = "http://127.0.0.1:8000/api/v1/users";
+export const usersEndpoint = "https://dummyjson.com/users";
